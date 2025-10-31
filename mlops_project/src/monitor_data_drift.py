eec498bd8779fa3bdc9d03cf95f26150b803a59f
david <202412026@urp.edu.pe>
2025-10-31 09:08:56 -0500

    Added data drift monitoring stage and HTML report

--- mlops_project/src/monitor_data_drift.py
+++ mlops_project/src/monitor_data_drift.py
@@ -3,12 +3,12 @@ from evidently.metric_preset import DataDriftPreset
 from evidently.report import Report
 
 def monitor_drift():
-    reference = pd.read_csv('data/processed/train.csv')
-    current = pd.read_csv('data/processed/test.csv')
+    reference = pd.read_csv('mlops_project/data/processed/train.csv')
+    current = pd.read_csv('mlops_project/data/processed/test.csv')
 
     report = Report(metrics=[DataDriftPreset()])
     report.run(reference_data=reference, current_data=current)
-    report.save_html('data_drift_report.html')
+    report.save_html('mlops_project/reports/data_drift_report.html')
     print("Data drift report saved as HTML.")
 
 if __name__ == "__main__":
